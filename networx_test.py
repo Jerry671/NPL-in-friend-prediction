96f281d9fe68d030e4ed8824e469ea3e8446c429
godzyp <1229460941@qq.com>
2019-10-25 22:22:43 +0800

    generate_pajek

--- networx_test.py
+++ networx_test.py
@@ -1,14 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import networkx.readwrite.pajek
 G=nx.Graph()
-filename="F:\\git\\NRP\\soc-pokec-relationships.txt"
-#filename="F:\\git\\NRP\\test.txt"
+#filename="F:\\git\\NRP\\soc-pokec-relationships.txt"
+filename="F:\\git\\NRP\\test.txt"
 edges = pd.read_table(filename, header=None, sep='\n')
 for i in range(len(edges)):
     coordinate = edges[0][i].split()
     G.add_edge(coordinate[0],coordinate[1])
 print(G.number_of_nodes())
+print(networkx.readwrite.pajek.generate_pajek(G))
+
 #nx.draw(G)
 #plt.show()
