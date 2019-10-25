ae8e0fe98a27ac45e22bb1c4938e4bc195248497
godzyp <1229460941@qq.com>
2019-10-25 22:05:58 +0800

    networkx

--- networx_test.py
+++ networx_test.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 G=nx.Graph()
-#filename="F:\\git\\NRP\\soc-pokec-relationships.txt"
-filename="F:\\git\\NRP\\test.txt"
-edges = pd.read_table(filename, header=None, sep='\t')
-for i in range(len(edges)):
+filename="F:\\git\\NRP\\soc-pokec-relationships.txt"
+#filename="F:\\git\\NRP\\test.txt"
+edges = pd.read_table(filename, header=None, sep='\n')
+for i in range(30622564):
     coordinate = edges[0][i].split()
     G.add_edge(coordinate[0],coordinate[1])
 print(G.number_of_nodes())
