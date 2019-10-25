05580b00f9fc65e0b9256cacdc4b097463048c74
godzyp <1229460941@qq.com>
2019-10-25 21:12:35 +0800

    test

---
+++ networx_test.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+
+G=nx.Graph()
+#filename="F:\\git\\NRP\\soc-pokec-relationships.txt"
+filename="F:\\git\\NRP\\test.txt"
+edges = pd.read_table(filename, header=None, sep='\t')
+for i in range(len(edges)):
+    coordinate = edges[0][i].split()
+    G.add_edge(coordinate[0],coordinate[1])
+print(G.number_of_nodes())
+nx.draw(G)
+plt.show()
